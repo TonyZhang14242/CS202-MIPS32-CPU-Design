3ec61bbc78954bf74dc87c34aa230ee2744f7f2a
ZHANG Zhentao <95693163+TonyZhang14242@users.noreply.github.com>
2022-04-27 20:06:21 +0800

    Add files via upload

---
+++ CPU.srcs/sources_1/ip/cpuclk/cpuclk_stub.v
@@ -0,0 +1,19 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+// Date        : Wed Apr 27 14:21:34 2022
+// Host        : LAPTOP-KSU6QTEM running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub d:/ComputerDesignLab/CPU/CPU.srcs/sources_1/ip/cpuclk/cpuclk_stub.v
+// Design      : cpuclk
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tfgg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module cpuclk(clk_out1, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_in1" */;
+  output clk_out1;
+  input clk_in1;
+endmodule
---
+++ CPU.srcs/sources_1/ip/cpuclk/cpuclk_stub.vhdl
@@ -0,0 +1,28 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Wed Apr 27 14:21:34 2022
+-- Host        : LAPTOP-KSU6QTEM running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub d:/ComputerDesignLab/CPU/CPU.srcs/sources_1/ip/cpuclk/cpuclk_stub.vhdl
+-- Design      : cpuclk
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tfgg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity cpuclk is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end cpuclk;
+
+architecture stub of cpuclk is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
