19c8d41595cd2ba1dc5214e5ae6e120dc6bfad32
YuZi0213 <77252090+YuZi0213@users.noreply.github.com>
2022-05-10 14:02:15 +0800

    Update control32.v

--- CPU.srcs/sources_1/new/control32.v
+++ CPU.srcs/sources_1/new/control32.v
@@ -38,23 +38,23 @@ output I_format,
 output Sftmd, // 1 indicate the instruction is shift instruction
 output [1:0]  ALUOp
 /* if the instruction is R-type or I_format, ALUOpis2'b10;
-if the instruction is"beq" or "bne", ALUOpis 2'b01£»if the instruction is"lw" or "sw", ALUOpis 2'b00£»*/
+if the instruction is"beq" or "bne", ALUOpis 2'b01Â£Â»if the instruction is"lw" or "sw", ALUOpis 2'b00Â£Â»*/
     );
     wire R_format;
    
     assign    Jr =((Opcode==6'b000000)&&(Function_opcode==6'b001000)) ? 1'b1 : 1'b0;
-     assign   Jmp= (Opcode==6'b000010) ? 1'b1 : 1'b0;
-     assign   Jal= (Opcode==6'b000011) ? 1'b1 : 1'b0;
+    assign   Jmp= (Opcode==6'b000010) ? 1'b1 : 1'b0;
+    assign   Jal= (Opcode==6'b000011) ? 1'b1 : 1'b0;
     assign    Branch= (Opcode==6'b000100) ? 1'b1 : 1'b0;
-     assign   nBranch= (Opcode==6'b000101) ? 1'b1 : 1'b0;
-     assign   MemtoReg= (Opcode==6'b100011) ? 1'b1 : 1'b0;
-     assign   R_format=(Opcode==6'b000000)? 1'b1:1'b0;
-     assign   MemWrite= (Opcode==6'b101011) ? 1'b1 : 1'b0;
-     assign   I_format = (Opcode[5:3]==3'b001)?1'b1:1'b0;
-     assign   Sftmd= (((Function_opcode==6'b000000)||(Function_opcode==6'b000010)||(Function_opcode==6'b000011)||(Function_opcode==6'b000100)||(Function_opcode==6'b000110)||(Function_opcode==6'b000111))&& R_format)? 1'b1:1'b0;
- 
+    assign   nBranch= (Opcode==6'b000101) ? 1'b1 : 1'b0;
+    assign   MemtoReg= (Opcode==6'b100011) ? 1'b1 : 1'b0;
+    assign   R_format=(Opcode==6'b000000)? 1'b1:1'b0;
+    assign   MemWrite= (Opcode==6'b101011) ? 1'b1 : 1'b0;
+    assign   I_format = (Opcode[5:3]==3'b001)?1'b1:1'b0;
+    assign   Sftmd= (((Function_opcode==6'b000000)||(Function_opcode==6'b000010)||(Function_opcode==6'b000011)||(Function_opcode==6'b000100)||(Function_opcode==6'b000110)||(Function_opcode==6'b000111))&& R_format)? 1'b1:1'b0;
+
     assign    RegDST=R_format;
-     assign   RegWrite = (R_format || MemtoReg || Jal || I_format) &&!(Jr);
-     assign   ALUSrc= (I_format||Opcode==6'b100011||Opcode==6'b101011);
-      assign  ALUOp = {(R_format || I_format),(Branch || nBranch)};
+    assign   RegWrite = (R_format || MemtoReg || Jal || I_format) &&!(Jr);
+    assign   ALUSrc= (I_format||Opcode==6'b100011||Opcode==6'b101011);
+    assign  ALUOp = {(R_format || I_format),(Branch || nBranch)};
 endmodule
